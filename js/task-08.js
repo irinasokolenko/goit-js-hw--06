4093e6214d9bc9050ea91aacfa9dbb419d9e151d
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-03 16:04:49 +0300

    hw06js

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,22 @@
+// const button = document.querySelector("button");
+// const form = document.querySelector("form");
+// form.addEventListener("submit", handleSubmit);
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password }
+//   } = event.currentTarget;
+//   if (email.value === "" || password.value === "") {
+//     return console.log("Please fill in all the fields!");
+//   } console.log(`Login: ${email.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// };
 loginForm.addEventListener('submit', handleFormSubmit);
-
 function handleFormSubmit(e) {
   e.preventDefault();
   const {
     elements: { email, password },
   } = e.currentTarget;
-
   if (!email.value.trim() || !password.value.trim()) {
     return alert('Please fill in all the fields!');
   } else {
