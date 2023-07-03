4980ebf5005ba99ff0f82e9ddf7e3b67e9172a06
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-03 16:07:56 +0300

    hw06js

--- js/task-08.js
+++ js/task-08.js
@@ -11,20 +11,37 @@
 //   } console.log(`Login: ${email.value}, Password: ${password.value}`);
 //   event.currentTarget.reset();
 // };
-loginForm.addEventListener('submit', handleFormSubmit);
-function handleFormSubmit(e) {
-  e.preventDefault();
-  const {
-    elements: { email, password },
-  } = e.currentTarget;
-  if (!email.value.trim() || !password.value.trim()) {
-    return alert('Please fill in all the fields!');
-  } else {
+// loginForm.addEventListener('submit', handleFormSubmit);
+// function handleFormSubmit(e) {
+//   e.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = e.currentTarget;
+//   if (!email.value.trim() || !password.value.trim()) {
+//     return alert('Please fill in all the fields!');
+//   } else {
+//     const formData = {
+//       email: email.value,
+//       password: password.value,
+//     };
+//     console.log(formData);
+//     e.currentTarget.reset();
+//   }
+// }
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(el) {
+    el.preventDefault();
+    const formElement = el.currentTarget.elements;
+    const email = formElement.email.value;
+    const password = formElement.password.value;
+    if (email === "" || password === "") {
+        return alert("Please fill in all the fields!");
+    }
     const formData = {
-      email: email.value,
-      password: password.value,
+        email,
+        password,
     };
     console.log(formData);
-    e.currentTarget.reset();
-  }
+    el.currentTarget.reset();
 }
