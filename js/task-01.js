ede6104212044a2d6cfba3ed82e30fa3a1379184
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-03 15:40:45 +0300

    hw06js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
 "use strict";
 
-const categoryItems = document.querySelectorAll("#categories > li.item");
-const itemsCount = categoryItems.length;
-console.log(`Number of categories: ${ itemsCount } `);
-categoryItems.forEach(category => {
-  const categoryName = category.querySelector("h2").textContent;
-  const categoryElements = category.querySelectorAll("li").length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElements}`);
-});
- 
+const categoriesRef = document.querySelector("#categories")
+const itemCategoriesRef = document.querySelectorAll('.item')
+
+console.log('Number of categories:', categoriesRef.children.length)
+
+const el = itemCategoriesRef.forEach(elem => {
+    console.log('Category:', elem.querySelector('h2').textContent)
+    
+    console.log('Elements:', elem.querySelector('ul').children.length);
+})
--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,19 @@
-const button = document.querySelector("button");
-const form = document.querySelector("form");
-form.addEventListener("submit", handleSubmit);
-function handleSubmit(event) {
-  event.preventDefault();
+loginForm.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(e) {
+  e.preventDefault();
   const {
-    elements: { email, password }
-  } = event.currentTarget;
-  if (email.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
-  } console.log(`Login: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-};
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (!email.value.trim() || !password.value.trim()) {
+    return alert('Please fill in all the fields!');
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    e.currentTarget.reset();
+  }
+}
